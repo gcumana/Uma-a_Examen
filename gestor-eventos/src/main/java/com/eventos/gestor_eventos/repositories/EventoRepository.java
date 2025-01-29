3e9df58d75e14d5a06f8890719820125b0dacc91
Umaña Lasluisa Giuliana Cristina <152335025+gcumana@users.noreply.github.com>
2025-01-29 11:40:24 -0500

    Delete gestor-eventos directory

--- gestor-eventos/src/main/java/com/eventos/gestor_eventos/repositories/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eventos.gestor_eventos.repositories;
-
-
-import com.eventos.gestor_eventos.models.entities.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
